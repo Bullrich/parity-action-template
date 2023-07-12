a2b4a79ed683aef7fe3443058472bd2121c44487
Javier Bullrich <javier@bullrich.dev>
2023-07-12 16:14:57 +0200

    added minimal code
    This adds enough code to build a basic project and some basic CI

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { getInput, info, setOutput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+import { Context } from "@actions/github/lib/context";
+
+const getRepo = (ctx: Context) => {
+  let repo = getInput("repo", { required: false });
+  if (!repo) {
+    repo = ctx.repo.repo;
+  }
+
+  let owner = getInput("owner", { required: false });
+  if (!owner) {
+    owner = ctx.repo.owner;
+  }
+
+  return { repo, owner };
+};
+
+const repo = getRepo(context);
+
+setOutput("repo", `${repo.owner}/${repo.repo}`);
